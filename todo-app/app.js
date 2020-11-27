ce4b2fe78213b74b43fb37b4f9c4fd09834ec92c
lonebots <jishnusurajila@gmail.com>
2020-11-27 17:30:54 +0530

    server setup complete

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,1 +1,21 @@
 const express=require('express');
+const app=express();
+
+//setting the view 
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+//request send  
+    console.log('request sent from: '+ req.url);
+
+//handling he request and responding to it    
+    res.writeHead(200, { 'Content-Type': 'text' });
+    res.end("you are all set to go");
+})
+
+
+//set the port
+app.listen(3000, (err)=>{
+    if(err)console.log(err);
+    else console.log('listening to port : 3000');
+})
