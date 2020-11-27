24c3b865850398425f1827b0382df68ee1ed1044
lonebots <jishnusurajila@gmail.com>
2020-11-28 00:06:55 +0530

    added mongoose database

--- todo-app/controllers/todoController.js
+++ todo-app/controllers/todoController.js
@@ -1,6 +1,21 @@
-var bodyParser =require('body-parser');
-var urlencodedParser =bodyParser.urlencoded({extended : false});//middileware
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });//middileware
 
+//mongoose 
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('')
+
+//create a schema - like a blue print 
+var todoSchema = new mongoose.Schema({ item: String });
+
+//create a model - stored as a collection in mongodb
+var Todo =mongoose.model('Todo',todoSchema);
+var itemOne =Todo({item : 'buy some pookals!'}).save((err)=>{//new data
+    if(err) throw err;
+    console.log('item saved');
+});//creating a data object
 
 
 //dummy data 
@@ -11,29 +26,29 @@ var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'todo app undakn
 //just a module which we export at the end
 module.exports = (app) => {
 
-   
+
     app.get('/todo', (req, res) => {
         res.render('todo', { todos: data });
     });
 
     //post
-    app.post('/todo',urlencodedParser,(req,res)=>{
+    app.post('/todo', urlencodedParser, (req, res) => {
         console.log("post call-back! started");
-        
+
         //push the data 
-            data.push(req.body);
+        data.push(req.body);
 
         //sending data back to the frontend 
         res.json(data);
-            
+
         console.log("post call-back! terminated");
     })
 
     //delete
     app.delete('/todo/:item', (req, res) => {
         //using a fliter function
-        data = data.filter((todo)=>{
-            return todo.item.replace(/ /g, '-')!==req.params.item;
+        data = data.filter((todo) => {
+            return todo.item.replace(/ /g, '-') !== req.params.item;
         })
         res.json(data);
     });
